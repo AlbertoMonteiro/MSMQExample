b14324a3892b433803a66ba2efd901d996d38fc8
Alberto Monteiro <alberto.monteiro@live.com>
2015-07-05 12:11:22 -0300

    fist source commit

---
+++ src/MsmqExample.Client1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+using ColoredConsole;
+
+namespace MsmqExample.Client1
+{
+    class Program
+    {
+        const string QUEUE_NAME = @".\private$\sample_queue";
+        static void Main(string[] args)
+        {
+            Console.Title = "Receiver";
+            RunConsumer(s => s.Red(), "Worker 1");
+            RunConsumer(s => s.White(), "Worker 2");
+            Console.ReadLine();
+        }
+
+        private static void RunConsumer(Func<string, ColorToken> inRed, string workerName)
+        {
+            Task.Run(() =>
+            {
+                var queue = new MessageQueue(QUEUE_NAME) {Formatter = new XmlMessageFormatter(new[] {typeof (string)})};
+                ColorConsole.WriteLine(inRed(string.Format("{0} - Iniciado", workerName)));
+                while (true)
+                {
+                    ColorConsole.WriteLine(inRed(string.Format("{0} - Carregando mensagens", workerName)));
+                    var message = queue.Receive();
+                    {
+                        //queue.
+                        if (message != null)
+                            ColorConsole.WriteLine(inRed(string.Format("{0} - Received: {1}", workerName, message.Body)));
+                    }
+                    ColorConsole.WriteLine(inRed(string.Format("{0} - Tudo recebido", workerName)));
+                    Thread.Sleep(800);
+                }
+            });
+        }
+    }
+}
---
+++ src/MsmqExample.Client2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Messaging;
+using System.Threading;
+
+namespace MsmqExample.Client2
+{
+    class Program
+    {
+        const string queue_name = @".\private$\sample_queue";
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Sender";
+
+            var queue = MessageQueue.Exists(queue_name) ? new MessageQueue(queue_name) : MessageQueue.Create(queue_name);
+
+            for (int i = 0; i < 100; i++)
+            {
+                queue.Send(i.ToString());
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
